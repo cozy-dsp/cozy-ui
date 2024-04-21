33ded42b6c8550c812850e9d77238cfb39a8d8ca
joe <hello@murl.digital>
2024-04-20 18:46:54 -0600

    rust iterators are actually cracked

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,6 +2,38 @@ use std::f32::consts::PI;
 
 use egui::{epaint::CubicBezierShape, Color32, Painter, Pos2, Shape, Stroke, Vec2};
 
+struct AngleIter {
+    start: Option<f32>,
+    end: f32
+}
+
+impl AngleIter {
+    const fn new(start_angle: f32, end_angle: f32) -> Self {
+        Self {
+            start: Some(start_angle),
+            end: end_angle
+        }
+    }
+}
+
+impl Iterator for AngleIter {
+    type Item = (f32, f32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.start.map(|start| {
+            let diff = self.end - start;
+            if diff.abs() < PI / 2.0 {
+                self.start = None;
+                (start, self.end)
+            } else {
+                let new_start = start + ((PI / 2.0) * diff.signum());
+                self.start = Some(new_start);
+                (start, new_start)
+            }
+        })
+    }
+}
+
 pub fn generate_arc(
     painter: &Painter,
     center: Pos2,
@@ -10,58 +42,32 @@ pub fn generate_arc(
     end_angle: f32,
     stroke: impl Into<Stroke>,
 ) {
-    let mut result = Vec::new();
-    recurse_arcs(
-        &mut result,
-        center,
-        radius,
-        start_angle,
-        end_angle,
-        stroke.into(),
-    );
-    painter.extend(result);
-}
+    let stroke = stroke.into();
+    painter.extend(AngleIter::new(start_angle, end_angle).map(|(start_angle, end_angle)| {
+        // Center of the circle
+        let xc = center.x;
+        let yc = center.y;
 
-fn recurse_arcs(
-    list: &mut Vec<Shape>,
-    center: Pos2,
-    radius: f32,
-    start_angle: f32,
-    end_angle: f32,
-    stroke: Stroke,
-) {
-    let diff = end_angle - start_angle;
-    let (start_angle, end_angle) = if diff.abs() < PI / 2.0 {
-        (start_angle, end_angle)
-    } else {
-        let new_start_angle = start_angle + ((PI / 2.0) * diff.signum());
-        recurse_arcs(list, center, radius, new_start_angle, end_angle, stroke);
-        (start_angle, new_start_angle)
-    };
+        // First control point
+        let p1 = center + radius * Vec2::new(start_angle.cos(), -start_angle.sin());
 
-    // Center of the circle
-    let xc = center.x;
-    let yc = center.y;
+        // Last control point
+        let p4 = center + radius * Vec2::new(end_angle.cos(), -end_angle.sin());
 
-    // First control point
-    let p1 = center + radius * Vec2::new(start_angle.cos(), -start_angle.sin());
+        let a = p1 - center;
+        let b = p4 - center;
+        let q1 = a.length_sq();
+        let q2 = q1 + a.dot(b);
+        let k2 = (4.0 / 3.0) * ((2.0 * q1 * q2).sqrt() - q2) / (a.x * b.y - a.y * b.x);
 
-    // Last control point
-    let p4 = center + radius * Vec2::new(end_angle.cos(), -end_angle.sin());
+        let p2 = Pos2::new(xc + a.x - k2 * a.y, yc + a.y + k2 * a.x);
+        let p3 = Pos2::new(xc + b.x + k2 * b.y, yc + b.y - k2 * b.x);
 
-    let a = p1 - center;
-    let b = p4 - center;
-    let q1 = a.length_sq();
-    let q2 = q1 + a.dot(b);
-    let k2 = (4.0 / 3.0) * ((2.0 * q1 * q2).sqrt() - q2) / (a.x * b.y - a.y * b.x);
-
-    let p2 = Pos2::new(xc + a.x - k2 * a.y, yc + a.y + k2 * a.x);
-    let p3 = Pos2::new(xc + b.x + k2 * b.y, yc + b.y - k2 * b.x);
-
-    list.push(Shape::CubicBezier(CubicBezierShape::from_points_stroke(
-        [p1, p2, p3, p4],
-        false,
-        Color32::TRANSPARENT,
-        stroke,
-    )));
+        Shape::CubicBezier(CubicBezierShape::from_points_stroke(
+            [p1, p2, p3, p4],
+            false,
+            Color32::TRANSPARENT,
+            stroke,
+        ))
+    }));
 }

da73a9c2330595bc00f9939301ed5ccb2ba290d9
joe <hello@murl.digital>
2024-04-20 21:46:44 -0600

    small refactors, and add iterator size hint for speed

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,16 +2,18 @@ use std::f32::consts::PI;
 
 use egui::{epaint::CubicBezierShape, Color32, Painter, Pos2, Shape, Stroke, Vec2};
 
+const PI_OVER_2: f32 = PI / 2.0;
+
 struct AngleIter {
     start: Option<f32>,
-    end: f32
+    end: f32,
 }
 
 impl AngleIter {
     const fn new(start_angle: f32, end_angle: f32) -> Self {
         Self {
             start: Some(start_angle),
-            end: end_angle
+            end: end_angle,
         }
     }
 }
@@ -22,15 +24,23 @@ impl Iterator for AngleIter {
     fn next(&mut self) -> Option<Self::Item> {
         self.start.map(|start| {
             let diff = self.end - start;
-            if diff.abs() < PI / 2.0 {
+            if diff.abs() < PI_OVER_2 {
                 self.start = None;
                 (start, self.end)
             } else {
-                let new_start = start + ((PI / 2.0) * diff.signum());
+                let new_start = start + (PI_OVER_2 * diff.signum());
                 self.start = Some(new_start);
                 (start, new_start)
             }
         })
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (
+            0,
+            self.start
+                .map(|start| ((self.end - start).abs() / PI_OVER_2).ceil() as usize),
+        )
     }
 }
 
@@ -43,31 +53,33 @@ pub fn generate_arc(
     stroke: impl Into<Stroke>,
 ) {
     let stroke = stroke.into();
-    painter.extend(AngleIter::new(start_angle, end_angle).map(|(start_angle, end_angle)| {
-        // Center of the circle
-        let xc = center.x;
-        let yc = center.y;
+    painter.extend(
+        AngleIter::new(start_angle, end_angle).map(|(start_angle, end_angle)| {
+            // Center of the circle
+            let xc = center.x;
+            let yc = center.y;
 
-        // First control point
-        let p1 = center + radius * Vec2::new(start_angle.cos(), -start_angle.sin());
+            // First control point
+            let p1 = center + radius * Vec2::new(start_angle.cos(), -start_angle.sin());
 
-        // Last control point
-        let p4 = center + radius * Vec2::new(end_angle.cos(), -end_angle.sin());
+            // Last control point
+            let p4 = center + radius * Vec2::new(end_angle.cos(), -end_angle.sin());
 
-        let a = p1 - center;
-        let b = p4 - center;
-        let q1 = a.length_sq();
-        let q2 = q1 + a.dot(b);
-        let k2 = (4.0 / 3.0) * ((2.0 * q1 * q2).sqrt() - q2) / (a.x * b.y - a.y * b.x);
+            let a = p1 - center;
+            let b = p4 - center;
+            let q1 = a.length_sq();
+            let q2 = q1 + a.dot(b);
+            let k2 = (4.0 / 3.0) * ((2.0 * q1 * q2).sqrt() - q2) / (a.x * b.y - a.y * b.x);
 
-        let p2 = Pos2::new(xc + a.x - k2 * a.y, yc + a.y + k2 * a.x);
-        let p3 = Pos2::new(xc + b.x + k2 * b.y, yc + b.y - k2 * b.x);
+            let p2 = Pos2::new(xc + a.x - k2 * a.y, yc + a.y + k2 * a.x);
+            let p3 = Pos2::new(xc + b.x + k2 * b.y, yc + b.y - k2 * b.x);
 
-        Shape::CubicBezier(CubicBezierShape::from_points_stroke(
-            [p1, p2, p3, p4],
-            false,
-            Color32::TRANSPARENT,
-            stroke,
-        ))
-    }));
+            Shape::CubicBezier(CubicBezierShape::from_points_stroke(
+                [p1, p2, p3, p4],
+                false,
+                Color32::TRANSPARENT,
+                stroke,
+            ))
+        }),
+    );
 }
